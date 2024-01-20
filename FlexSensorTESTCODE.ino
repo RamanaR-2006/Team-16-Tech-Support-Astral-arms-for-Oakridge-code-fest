e9401c7cf08d1bd0ee7a13d0b3280d9ce7c0446c
wanderAM05 <67002944+wanderAM05@users.noreply.github.com>
2024-01-20 15:52:34 +0530

    Update FlexSensorTESTCODE.ino

--- FlexSensorTESTCODE.ino
+++ FlexSensorTESTCODE.ino
@@ -4,8 +4,8 @@
 float VCC = 5; // arduino is powered with 5V VCC pins
 float R2 = 10000; // two 10K resistors are used
 // property of the flex sensor (what I refer to as the Flex later on) is that as it is bent, resistance should increase (bent in one direction only). Flat flex sensor = least resistance)
-float sensorMinResistance = 16700; // value of the Flex when its flat
-float sensorMaxResistance = 18200; // value of the Flex when its bent (max degrees = 90)
+float minResistance = 16700; // value of the Flex when its flat
+float maxResistance = 18200; // value of the Flex when its bent (max degrees = 90)
 
 
 void setup() {
@@ -19,7 +19,7 @@ void loop() {
   int ADCRaw = analogRead(sensorPin);
   float ADCVoltage = (ADCRaw * VCC) / 1023; // get the voltage value
   float Resistance = R2 * (VCC / ADCVoltage - 1); // calculate resistance as you bend/keep it flat
-  uint8_t ReadValue = map(Resistance, sensorMinResistance, sensorMaxResistance, 0, 255); // map the values 16700 to 0  18200 to 255
+  uint8_t ReadValue = map(Resistance, minResistance, maxResistance, 0, 255); // map the values 16700 to 0  18200 to 255
   analogWrite(PWMPin, ReadValue); // Generate PWM Signal
 
   // information for debugging 
